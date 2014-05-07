65288fa2874b675087cba62229324977bcbd5f05
mani sintan <mani.jhupro@gmail.com>
2014-05-07 16:47:13 +0545

    micropost created

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 SampleApp::Application.routes.draw do
  # get "users/new"
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   root to: 'staticpages#home'
 
   match '/signup',  to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match '/help',    to: 'staticpages#help'
   match '/about',   to: 'staticpages#about'
